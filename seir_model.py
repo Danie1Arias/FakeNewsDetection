878c9ed91b68bb241acd3b343d8b7c79e3be110c
Danie1Arias <daniel.arias@estudiants.urv.cat>
2025-05-20 17:02:58 +0200

    SEIR Custom model

--- seir_model.py
+++ seir_model.py
@@ -1,16 +1,22 @@
-# seir_model.py
 import numpy as np
-import networkx as nx
 import matplotlib.pyplot as plt
 
 class SEIRFakeNewsModel:
-    def __init__(self, G, beta, delta, gamma, alpha, eta, initial_state=None):
+    def __init__(self, G, beta, delta, mu, gamma, eta, initial_state=None):
+        """
+        G: networkx graph
+        beta: infection/contact rate
+        delta: decision rate (1/δ = avg. time to decide)
+        mu: probability of an I node becoming R
+        gamma: probability of an R node reverting to I
+        eta: array of length N (per node probability of not spreading fake news)
+        """
         self.G = G
         self.N = G.number_of_nodes()
         self.beta = beta
         self.delta = delta
+        self.mu = mu
         self.gamma = gamma
-        self.alpha = alpha
         self.eta = np.array(eta)
         self.states = {node: 'S' for node in G.nodes}
         if initial_state:
@@ -19,20 +25,23 @@ class SEIRFakeNewsModel:
     def step(self):
         new_states = self.states.copy()
         for i in self.G.nodes:
-            if self.states[i] == 'S':
-                for j in self.G.neighbors(i):
-                    if self.states[j] == 'I' and np.random.rand() < self.beta:
-                        new_states[i] = 'E'
-                        break
-            elif self.states[i] == 'E':
+            state = self.states[i]
+            if state == 'S':
+                if any(self.states[j] == 'I' and np.random.rand() < self.beta for j in self.G.neighbors(i)):
+                    new_states[i] = 'E'
+            elif state == 'E':
                 if np.random.rand() < self.delta:
                     new_states[i] = 'R' if np.random.rand() < self.eta[i] else 'I'
-            elif self.states[i] == 'I':
+            elif state == 'I':
+                if np.random.rand() < self.mu:
+                    new_states[i] = 'R'
+            elif state == 'R':
                 if np.random.rand() < self.gamma:
-                    new_states[i] = 'R' if np.random.rand() < self.alpha else 'S'
+                    new_states[i] = 'I'
         self.states = new_states
 
-    def run(self, Tmax):
+
+    def run(self, Tmax=100):
         history = []
         for _ in range(Tmax):
             counts = self.count_states()
@@ -46,26 +55,28 @@ class SEIRFakeNewsModel:
             count[state] += 1
         return count
 
-def simulate_seir(G, beta, delta, gamma, alpha, eta, initial_state, Tmax=100):
-    model = SEIRFakeNewsModel(G, beta, delta, gamma, alpha, eta, initial_state)
+
+def simulate_seir(G, beta, delta, mu, gamma, eta, initial_state, Tmax=100):
+    model = SEIRFakeNewsModel(G, beta, delta, mu, gamma, eta, initial_state)
     return model.run(Tmax)
 
-def plot_seir(history, title='SEIR Model'):
+
+def plot_seir(history, title='SEIR Simulation'):
     S, E, I, R = [], [], [], []
     for step in history:
         S.append(step['S'])
         E.append(step['E'])
         I.append(step['I'])
         R.append(step['R'])
-
-    plt.figure(figsize=(10, 6))
+    
+    plt.figure(figsize=(10,6))
     plt.plot(S, label='Susceptible (S)')
     plt.plot(E, label='Exposed (E)')
-    plt.plot(I, label='Infected (I - Spreading fake)')
-    plt.plot(R, label='Recovered (R)')
-    plt.xlabel('Time step')
-    plt.ylabel('Number of nodes')
+    plt.plot(I, label='Infected (I - Spreading)')
+    plt.plot(R, label='Removed (R)')
     plt.title(title)
-    plt.legend()
+    plt.xlabel("Time Step")
+    plt.ylabel("Number of Nodes")
     plt.grid(True)
+    plt.legend()
     plt.show()
